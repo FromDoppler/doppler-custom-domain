dd04f0f0fa66adb787b548ce0f0ee9ced5c7c69e
bamaro <amaro.belen.a@gmail.com>
2020-08-12 11:31:52 -0300

    Add super user validation logging

--- DopplerCustomDomain/DopplerSecurity/IsSuperUserHandler.cs
+++ DopplerCustomDomain/DopplerSecurity/IsSuperUserHandler.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,19 @@ namespace DopplerCustomDomain.DopplerSecurity
 {
     public class IsSuperUserHandler : AuthorizationHandler<IsSuperUserRequirement>
     {
+        private readonly ILogger<IsSuperUserHandler> _logger;
+
+        public IsSuperUserHandler(ILogger<IsSuperUserHandler> logger)
+        {
+            _logger = logger;
+        }
+
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
             IsSuperUserRequirement requirement)
         {
             if (!context.User.HasClaim(c => c.Type.Equals("isSU")))
             {
+                _logger.LogWarning("The token hasn't super user permissions.");
                 return Task.CompletedTask;
             }
 
@@ -20,8 +29,10 @@ namespace DopplerCustomDomain.DopplerSecurity
             if (isSuperUser)
             {
                 context.Succeed(requirement);
+                return Task.CompletedTask;
             }
 
+            _logger.LogWarning("The token super user permissions is false.");
             return Task.CompletedTask;
         }
     }
