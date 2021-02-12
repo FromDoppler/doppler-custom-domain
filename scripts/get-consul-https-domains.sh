c60a4af2792f6188134fd9d92a50b8f05288a136
Andrés Moschini <andresmoschini@gmail.com>
2021-02-12 16:59:51 -0300

    chore: add a script to download the list of https domains in consul

---
+++ scripts/get-consul-https-domains.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+print_help () {
+    echo ""
+    echo "Usage: sh get-consul-https-domains.sh"
+    echo ""
+    echo "Get the list of registered domains in consul and return them as a list."
+    echo "Important: it requires to be connected to the VPN."
+    echo ""
+    echo "Examples:"
+    echo "  sh get-consul-https-domains.sh"
+    echo "  sh get-consul-https-domains.sh | sh verify-domains.sh > verified-domains.csv"
+    echo "  sh get-consul-https-domains.sh | sh verify-domains.sh | grep FAIL | cut -d',' -f 1 | sh delete-domains.sh"
+}
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+consulDomain=172.25.48.222:18500
+json=$(curl --request GET \
+  --url "http://${consulDomain}/v1/kv/traefik/http/routers/?keys" \
+  -s)
+
+echo "${json}" \
+  | tr ',' '\n' \
+  | grep -e '.*"traefik\/http\/routers\/https_\([^\/]*\)\/rule".*$' \
+  | sed 's/.*"traefik\/http\/routers\/https_\([^\/]*\)\/rule".*$/\1/'
