dee317057da648fd7db07df47ce90768c8902eb8
Andrés Moschini <andresmoschini@gmail.com>
2021-02-12 12:43:53 -0300

    chore: add a script to delete registered domains

---
+++ scripts/delete-domains.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+print_help () {
+    echo ""
+    echo "Usage: sh delete-domains.sh"
+    echo ""
+    echo "Read a list of domains from standard input, remove from custom domains API"
+    echo ""
+    echo "Examples:"
+    echo "  cat wrong-domains.txt | sh delete-domains.sh"
+}
+
+# TODO: complete the JWT token with an isSU one or do something to read it from an environment variable
+# token="eyJhbGciOiJSUzI1NiIsI...
+while read -r domain; do
+  echo "${domain}"
+  curl --request DELETE \
+    --url "https://apis.fromdoppler.com/routing/${domain}" \
+    --header "authorization: Bearer ${token}"
+  echo ""
+done
